f0dc806ceb5ef0d956d2771d02fb56caaa6ba5d7
Ajay Negi <ajnegi456@gmail.com>
2024-12-11 07:03:13 +0000

    strings

--- start.py
+++ start.py
@@ -1,4 +1,12 @@
-a=10
-b=20
-c=a+b
-print(c)
+# a=10
+# b=20
+# c=a+b
+# print(c)
+
+#Formated string
+#  first = "ajay"
+# last = "negi"
+# message = first + '['+last+']is a coder'
+# msg = f'{first} [{last}] is a coder'
+# print(message)
+# print(msg)
