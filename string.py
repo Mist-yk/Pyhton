fa11caa81470b610a4711a2a83cad7afc49db13f
Ajay Negi <ajnegi456@gmail.com>
2024-12-11 07:49:14 +0000

    Strings and if else

---
+++ string.py
@@ -0,0 +1,44 @@
+# a=10
+# b=20
+# c=a+b
+# print(c)
+
+#Formated string
+#  first = "ajay"
+# last = "negi"
+# message = first + '['+last+']is a coder'
+# msg = f'{first} [{last}] is a coder'
+# print(message)
+# print(msg)
+
+
+#functions (string)
+##len
+# name ="kam 25"
+# print(len(name))
+
+#print(name.upper())
+#print(name.lower())
+#print(name.find('a'))
+#print(name.replace('a','c'))
+#print('python' in name)
+
+
+
+
+#a=2.9
+#print(round(a))  #(will return round off value)
+#print(abs(-2.9)) #(will always return positive value)
+
+
+
+##if else
+# is_hot = False
+# is_cold = True
+# if is_hot:
+#     print("it's a hot day")
+
+# elif is_cold:
+#     print("it's a good day")
+# else:
+#     print("doctor pe ja")
